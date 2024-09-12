31e0e7f7b0312a9377d84bd9d80fb1946c37514b
OlenkaPasko <113981072+OlenkaPasko@users.noreply.github.com>
2024-09-13 00:54:14 +0300

    setupServer

--- src/server.js
+++ src/server.js
@@ -2,22 +2,27 @@
 import express from 'express';
 import cors from 'cors';
 import pino from 'pino-http';
+
 import { env } from './utils/env.js';
+
+//import * as contactServices from './services/contacts.js';
+import { getAllContact, getContactById } from './services/contacts.js';
 
 export const setupServer = () => {
   const app = express();
+
   const logger = pino({
     transport: {
       target: 'pino-pretty',
     },
   });
+
   app.use(logger);
   app.use(cors());
   app.use(express.json());
 
-  //routes
   app.get('/contacts', async (req, res) => {
-    const data = await contactServices.getAllContact();
+    const data = await getAllContact();
 
     res.json({
       status: 200,
@@ -25,9 +30,10 @@ export const setupServer = () => {
       data,
     });
   });
+
   app.get('/contacts/:id', async (req, res) => {
     const { id } = req.params;
-    const data = await contactServices.getContactById(id);
+    const data = await getContactById(id);
 
     if (!data) {
       return res.status(404).json({
@@ -44,20 +50,23 @@ export const setupServer = () => {
 
   app.use((req, res) => {
     res.status(404).json({
-      message: `${req.url}Not found`,
+      message: `${req.url} not found`,
     });
   });
-  //обробка помилок
 
-  app.use((err, req, res, next) => {
+  app.use((error, req, res, next) => {
     res.status(500).json({
       message: error.message,
     });
   });
-
   //запуск сервера
   const PORT = Number(env('PORT', 3000));
-  app.listen(PORT, () => console.log('Server is running on port {PORT}'));
+
+  app.listen(PORT, () => console.log('Server running on port 3000'));
 };
+
+  //запуск сервера
+  //const PORT = Number(env('PORT', 3000));
+  //app.listen(PORT, () => console.log('Server running on port 3000'));
 
 //req -це об'єкт який має інформацію про запит res - це об'єкт який отримує відповідь.res відправляє відповідб тому хто цей запити зробив
