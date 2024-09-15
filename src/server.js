e18abcf325bdaab3032ecd4c142ef98cc2411a82
OlenkaPasko <113981072+OlenkaPasko@users.noreply.github.com>
2024-09-15 17:11:44 +0300

    logger

--- src/server.js
+++ src/server.js
@@ -1,19 +1,16 @@
 //логіка роботи  express-серверу
 import express from 'express';
 import cors from 'cors';
-import pino from 'pino-http';
 
 import { env } from './utils/env.js';
 import contactsRouter from './routers/contacts.js';
 
+import notFoundHandler from './middlewares/notFoundHandler.js';
+import errorHandler from './middlewares/errorHandler.js';
+import logger from './middlewares/logger.js';
+
 export const setupServer = () => {
   const app = express();
-
-  const logger = pino({
-    transport: {
-      target: 'pino-pretty',
-    },
-  });
 
   app.use(logger);
   app.use(cors());
@@ -22,18 +19,9 @@ export const setupServer = () => {
   app.use('/contacts', contactsRouter); //якщо прийде будь який запит який читається/contacts то потрібно
   //шукати обробник запитів у цьому об'єкті contactsRouter!
 
-  app.use((req, res) => {
-    res.status(404).json({
-      message: `${req.url} not found`,
-    });
-  });
+  app.use(notFoundHandler);
+  app.use(errorHandler);
 
-  app.use((error, req, res, next) => {
-    const { status = 500, message } = error;
-    res.status(status).json({
-      message,
-    });
-  });
   //запуск сервера
   const PORT = Number(env('PORT', 3000));
 
