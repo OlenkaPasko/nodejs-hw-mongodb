e43f472a7a0ca38dc07e845c7e9dfb5b322d08cc
OlenkaPasko <113981072+OlenkaPasko@users.noreply.github.com>
2024-09-17 06:27:00 +0300

    errorHandler

--- src/middlewares/errorHandler.js
+++ src/middlewares/errorHandler.js
@@ -1,8 +1,18 @@
-const errorHandler = (error, req, res, next) => {
-  const { status = 500, message } = error;
-  res.status(status).json({
-    message,
+import { HttpError } from 'http-errors';
+const errorHandler = (err, req, res, next) => {
+  if (err instanceof HttpError) {
+    res.status(err.status).json({
+      status: err.status,
+      message: err.name,
+      data: err,
+    });
+    return;
+  }
+
+  res.status(500).json({
+    status: 500,
+    message: 'Something went wrong',
+    data: err.message,
   });
 };
-
 export default errorHandler;
