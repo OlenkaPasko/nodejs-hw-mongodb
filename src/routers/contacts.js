e766b46fbf928e09bd98b283ded5735b107f0c3b
OlenkaPasko <113981072+OlenkaPasko@users.noreply.github.com>
2024-09-19 15:18:42 +0300

    isValidId

--- src/routers/contacts.js
+++ src/routers/contacts.js
@@ -2,6 +2,13 @@ import { Router } from 'express';
 import * as contactControllers from '../controllers/contacts.js';
 import ctrlWrapper from '../utils/ctrlWrapper.js';
 
+import isValidId from '../middlewares/isValidId.js';
+import validateBody from '../utils/validateBody.js';
+import {
+  contactAddSchema,
+  contactPatchSchema,
+} from '../validation/contacts.js';
+
 const contactsRouter = Router(); //об'єкт в який можна додавати маршрут
 
 contactsRouter.get(
@@ -11,20 +18,30 @@ contactsRouter.get(
 
 contactsRouter.get(
   '/:id',
+  isValidId,
   ctrlWrapper(contactControllers.getContactByIdController),
 );
 
-contactsRouter.post('/', ctrlWrapper(contactControllers.addContactController));
+contactsRouter.post(
+  '/',
+  validateBody(contactAddSchema),
+  ctrlWrapper(contactControllers.addContactController),
+);
 contactsRouter.put(
   '/:id',
+  isValidId,
+  validateBody(contactAddSchema),
   ctrlWrapper(contactControllers.upsertContactController),
 );
 contactsRouter.patch(
   '/:id',
+  isValidId,
+  validateBody(contactPatchSchema),
   ctrlWrapper(contactControllers.patchContactController),
 );
 contactsRouter.delete(
   '/:id',
+  isValidId,
   ctrlWrapper(contactControllers.deleteContactController),
 );
 export default contactsRouter;
--- src/validation/contacts.js
+++ src/validation/contacts.js
@@ -3,10 +3,20 @@ import {genreList} from "../constants/contacts.js"
 
 export const contactAddSchema = Joi.object({
   name: Joi.string().min(3).max(20).required(),
-  phoneNumber: Joi.number().required(),
+  phoneNumber: Joi.string().required().messages({
+        "any.required": "Phone number is required",
+    }),
   email: Joi.string().email().required(),
-contactType: Joi.string().valid(...genreList).required(),
-
+  contactType: Joi.string()
+    .valid(...genreList)
+    .required(),
+  isFavourite: Joi.boolean()
 });
 
-
+export const contactPatchSchema = Joi.object({
+  name: Joi.string(),
+  phoneNumber: Joi.string(),
+  email: Joi.string(),
+  isFavourite: Joi.boolean(),
+  contactType: Joi.number().valid(...contactList),
+});
