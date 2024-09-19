ceac9229441a26d2648bde1693f2cebc783eb698
OlenkaPasko <113981072+OlenkaPasko@users.noreply.github.com>
2024-09-19 15:51:06 +0300

    genreList

--- src/validation/contacts.js
+++ src/validation/contacts.js
@@ -1,16 +1,16 @@
 import Joi from 'joi';
-import {genreList} from "../constants/contacts.js"
+import { genreList } from '../constants/contacts.js';
 
 export const contactAddSchema = Joi.object({
   name: Joi.string().min(3).max(20).required(),
   phoneNumber: Joi.string().required().messages({
-        "any.required": "Phone number is required",
-    }),
+    'any.required': 'Phone number is required',
+  }),
   email: Joi.string().email().required(),
   contactType: Joi.string()
     .valid(...genreList)
     .required(),
-  isFavourite: Joi.boolean()
+  isFavourite: Joi.boolean(),
 });
 
 export const contactPatchSchema = Joi.object({
@@ -18,5 +18,5 @@ export const contactPatchSchema = Joi.object({
   phoneNumber: Joi.string(),
   email: Joi.string(),
   isFavourite: Joi.boolean(),
-  contactType: Joi.number().valid(...contactList),
+  contactType: Joi.number().valid(...genreList),
 });
